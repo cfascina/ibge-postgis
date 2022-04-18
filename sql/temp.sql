0f21e1439bfa161b5d4405f6f031aaf6844ce4b9
cfascina <cfascina@gmail.com>
2022-04-18 14:53:46 -0300

    SQL auxiliar files

---
+++ sql/temp.sql
@@ -0,0 +1,2 @@
+select count(1) from public.populacao_residente_mas where cast(id_setor_censitario as text) like '29%';
+-- delete from public.populacao_residente_mas where cast(id_setor_censitario as text) like '29%';
