d454349558127a217dc5f3dfcefe77bdf16a9c7a
cfascina <cfascina@gmail.com>
2022-04-19 14:59:55 -0300

    Removes temp.sql

--- sql/temp.sql
+++
@@ -1,2 +0,0 @@
-select count(1) from public.populacao_residente_mas where cast(id_setor_censitario as text) like '29%';
--- delete from public.populacao_residente_mas where cast(id_setor_censitario as text) like '29%';
