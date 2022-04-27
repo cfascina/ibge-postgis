884872b56ec7568ab619bc6fbe7dd69173a8a0f2
cfascina <cfascina@gmail.com>
2022-04-27 17:53:15 -0300

    Checking queries

--- sql/checks.sql
+++ sql/checks.sql
@@ -1,13 +1,8 @@
--- SELECT COUNT(1) FROM public.responsavel_renda WHERE CAST(id_setor_censitario AS TEXT) LIKE '28%';
--- DELETE FROM public.responsavel_renda WHERE CAST(id_setor_censitario AS TEXT) SIMILAR TO '99%';
-
--- -- -- -- -- -- -- -- -- --
-
 -- SELECT
 -- (SELECT COUNT(1) FROM public.populacao_residente_mas WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as MAS, 
 -- (SELECT COUNT(1) FROM public.populacao_residente_fem WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as FEM;
+
 -- DELETE FROM public.populacao_residente_fem WHERE CAST(id_setor_censitario AS TEXT) SIMILAR TO '12%|29%|50%';
--- 3724
 
 -- -- -- -- -- -- -- -- -- --
 
@@ -15,6 +10,9 @@
 -- SELECT COUNT(1) FROM public.populacao_residente_mas; -- 310.120
 -- SELECT COUNT(1) FROM public.populacao_residente_fem; -- 310.120
 -- SELECT COUNT(1) FROM public.responsavel_renda; -- 310.120
+-- SELECT COUNT(1) FROM public.domicilio_renda; -- 310.120
+
+-- -- -- -- -- -- -- -- -- --
 
 -- SELECT COUNT(1) FROM public.setores_censitarios WHERE CAST(cod_ibge AS TEXT) LIKE '29%'; -- 24.139
 -- SELECT COUNT(1) FROM public.populacao_residente_mas WHERE CAST(id_setor_censitario AS TEXT) LIKE '29%';
