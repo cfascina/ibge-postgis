6d1fbec1ae4deb2d740a5e2ba1c9a536510e042a
cfascina <cfascina@gmail.com>
2022-04-28 16:47:36 -0300

    Checking queries

--- sql/checks.sql
+++ sql/checks.sql
@@ -1,4 +1,10 @@
 -- SELECT
+-- (SELECT COUNT(1) FROM public.setores_censitarios WHERE CAST(cod_ibge AS TEXT) LIKE '28%') AS SC,
+-- (SELECT COUNT(1) FROM public.indice_envelhecimento WHERE CAST(id_setor_censitario AS TEXT) LIKE '28%') AS IE;
+
+-- -- -- -- -- -- -- -- -- --
+
+-- SELECT
 -- (SELECT COUNT(1) FROM public.populacao_residente_mas WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as MAS, 
 -- (SELECT COUNT(1) FROM public.populacao_residente_fem WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as FEM;
 
@@ -6,14 +12,10 @@
 
 -- -- -- -- -- -- -- -- -- --
 
+-- INSERIR VALORES NULL PARA OS SETORES FALTANTES
 -- SELECT COUNT(1) FROM public.setores_censitarios; -- 316.574
+-- SELECT COUNT(1) FROM public.indice_envelhecimento; -- 316.574
 -- SELECT COUNT(1) FROM public.populacao_residente_mas; -- 310.120
 -- SELECT COUNT(1) FROM public.populacao_residente_fem; -- 310.120
 -- SELECT COUNT(1) FROM public.responsavel_renda; -- 310.120
 -- SELECT COUNT(1) FROM public.domicilio_renda; -- 310.120
-
--- -- -- -- -- -- -- -- -- --
-
--- SELECT COUNT(1) FROM public.setores_censitarios WHERE CAST(cod_ibge AS TEXT) LIKE '29%'; -- 24.139
--- SELECT COUNT(1) FROM public.populacao_residente_mas WHERE CAST(id_setor_censitario AS TEXT) LIKE '29%';
--- SELECT COUNT(1) FROM public.setores_censitarios WHERE CAST(cod_ibge AS TEXT) LIKE '290890300%';
