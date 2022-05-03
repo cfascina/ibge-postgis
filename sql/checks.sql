fa73b1d8ac84a91c81f43ec623783b703bbd4897
cfascina <cfascina@gmail.com>
2022-05-03 15:16:15 -0300

    Checking queries

--- sql/checks.sql
+++ sql/checks.sql
@@ -1,26 +1,16 @@
 -- SELECT
--- (SELECT COUNT(1) FROM public.setores_censitarios WHERE CAST(cod_ibge AS TEXT) LIKE '28%') AS SC,
--- (SELECT COUNT(1) FROM public.populacao_idade_ativa WHERE CAST(id_setor_censitario AS TEXT) LIKE '28%') AS PIA;
-
--- DELETE FROM public.populacao_idade_ativa WHERE CAST(id_setor_censitario AS TEXT) SIMILAR TO '12%|29%|50%';
-
--- -- -- -- -- -- -- -- -- --
-
--- SELECT
--- (SELECT COUNT(1) FROM public.populacao_residente_mas WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as MAS, 
--- (SELECT COUNT(1) FROM public.populacao_residente_fem WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as FEM;
+-- (SELECT COUNT(1) FROM public.populacao_residente_mas WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as PRM, 
+-- (SELECT COUNT(1) FROM public.populacao_residente_fem WHERE CAST(id_setor_censitario AS TEXT) LIKE '27%') as PRF;
 
 -- DELETE FROM public.populacao_residente_fem WHERE CAST(id_setor_censitario AS TEXT) SIMILAR TO '12%|29%|50%';
 
 -- -- -- -- -- -- -- -- -- --
 
--- INSERIR VALORES NULL PARA OS SETORES FALTANTES
 -- SELECT COUNT(1) FROM public.setores_censitarios; -- 316.574
 -- SELECT COUNT(1) FROM public.indice_envelhecimento; -- 316.574
 -- SELECT COUNT(1) FROM public.populacao_idade_ativa; -- 316.574
-
 -- SELECT COUNT(1) FROM public.populacao_residente_mas; -- 310.120
 -- SELECT COUNT(1) FROM public.populacao_residente_fem; -- 310.120
 -- SELECT COUNT(1) FROM public.responsavel_renda; -- 310.120
 -- SELECT COUNT(1) FROM public.domicilio_renda; -- 310.120
-
+-- SELECT COUNT(1) FROM public.razao_sexo; -- 310.120
